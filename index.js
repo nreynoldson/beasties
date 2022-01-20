10386c20f3d6dc1b0fa75f402638faad95166167
nreynoldson <nicoleareynoldson@gmail.com>
2022-01-19 22:27:10 -0800

    Testing the express api route on heroku

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // An api endpoint that returns a short list of items
 app.get('/api/', (req,res) => {
-
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
 });
 
 // Handles any requests that don't match the ones above
