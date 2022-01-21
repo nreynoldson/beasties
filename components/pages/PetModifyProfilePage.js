9a54f03c119a3fb26635b970760f1fc26aae27fc
kruegead <kruegead@oregonstate.edu>
2022-01-20 19:40:36 -0600

    Add cat breeds

--- components/pages/PetModifyProfilePage.js
+++ components/pages/PetModifyProfilePage.js
@@ -5,98 +5,118 @@ import './css/Common.css';
 import './css/PetModifyProfilePage.css';
 
 const dogBreedsMap = [
-  ["labrador", "Labrador Retriever"],
-  ["frenchBulldog", "French Bulldog"],
-  ["germanShepherdDog", "German Shepherd Dog"],
-  ["goldenRetriever", "Golden Retriever"],
-  ["bulldog", "Bulldog"],
-  ["poodle", "Poodle"],
-  ["beagle", "Beagle"],
-  ["rottweiler", "Rottweiler"],
-  ["germanShorthairedPointer", "German Shorthaired Pointer"],
-  ["dachshund", "Dachshund"],
-  ["pembrokeWelshCorgi", "Pembroke Welsh Corgi"],
-  ["australianShepherd", "Australian Shepherd"],
-  ["yorkshireTerrier", "Yorkshire Terrier"],
-  ["boxer", "Boxer"],
-  ["greatDane", "Great Dane"],
-  ["siberianHuskie", "Siberian Huskie"],
-  ["cavalierKingCharlesSpaniel", "Cavalier King Charles Spaniel"],
-  ["dobermanPinscher", "Doberman Pinscher"],
-  ["miniatureSchnauzer", "Miniature Schnauzer"],
-  ["shihTzu", "Shih Tzu"],
-  ["bostonTerrier", "Boston Terrier"],
-  ["berneseMountainDog", "Bernese Mountain Dog"],
-  ["pomeranian", "Pomeranian"],
-  ["havanese", "Havanese"],
-  ["caneCorso", "Cane Corso"],
-  ["englishSpringerSpaniel", "English Springer Spaniel"],
-  ["shetlandSheepdog", "Shetland Sheepdog"],
-  ["brittany", "Brittany"],
-  ["pug", "Pug"],
-  ["cockerSpaniel", "Cocker Spaniel"],
-  ["miniatureAmericanShepherd", "Miniature American Shepherd"],
-  ["borderCollie", "Border Collie"],
-  ["mastiff", "Mastiff"],
-  ["chihuahua", "Chihuahua"],
-  ["vizsla", "Vizsla"],
-  ["bassetHound", "Basset Hound"],
-  ["belgianMalinoi", "Belgian Malinoi"],
-  ["maltese", "Maltese"],
-  ["weimaraner", "Weimaraner"],
-  ["collie", "Collie"],
-  ["newfoundland", "Newfoundland"],
-  ["rhodesianRidgeback", "Rhodesian Ridgeback"],
-  ["shibaInu", "Shiba Inu"],
-  ["westHighlandWhiteTerrier", "West Highland White Terrier"],
-  ["bichonsFrise", "Bichons Frise"],
-  ["bloodhound", "Bloodhound"],
-  ["englishCockerSpaniels", "English Cocker Spaniel"],
-  ["akita", "Akita"],
-  ["portugueseWaterDog", "Portuguese Water Dog"],
-  ["chesapeakeBayRetriever", "Chesapeake Bay Retriever"],
-  ["dalmatian", "Dalmatian"],
-  ["stBernard", "St. Bernard"],
-  ["papillon", "Papillon"],
-  ["australianCattleDog", "Australian Cattle Dog"],
-  ["bullmastiff", "Bullmastiff"],
-  ["samoyed", "Samoyed"],
-  ["scottishTerrier", "Scottish Terrier"],
-  ["softCoatedWheatenTerrier", "Soft Coated Wheaten Terrier"],
-  ["whippet", "Whippet"],
-  ["germanWirehairedpointer", "German Wirehaired Pointer"],
-  ["chineseShar-Pei", "Chinese Shar-Pei"],
-  ["airedaleTerrier", "Airedale Terrier"],
-  ["wirehairedPointingGriffon", "Wirehaired Pointing Griffon"],
-  ["bullTerrier", "Bull Terrier"],
-  ["alaskanMalamute", "Alaskan Malamute"],
-  ["cardiganWelshCorgi", "Cardigan Welsh Corgi"],
-  ["giantSchnauzer", "Giant Schnauzer"],
-  ["oldEnglishSheepdog", "Old English Sheepdog"],
-  ["italianGreyhound", "Italian Greyhound"],
-  ["greatPyrenee", "Great Pyrenee"],
-  ["doguesdeBordeaux", "Dogues de Bordeaux"],
-  ["russellTerrier", "Russell Terrier"],
-  ["cairnTerrier", "Cairn Terrier"],
-  ["irishWolfhound", "Irish Wolfhound"],
-  ["irishsetter", "Irish Setters"],
-  ["greaterSwissMountainDog", "Greater Swiss Mountain Dog"],
-  ["miniaturePinscher", "Miniature Pinscher"],
-  ["lhasaApso", "Lhasa Apso"],
-  ["chineseCrested", "Chinese Crested"],
-  ["cotondeTulear", "Coton de Tulear"],
-  ["staffordshireBullTerrier", "Staffordshire Bull Terrier"],
-  ["americanStaffordshireTerrier", "American Staffordshire Terrier"],
-  ["ratTerrier", "Rat Terrier"],
-  ["chowChow", "Chow Chow"],
-  ["anatolianShepherdDog", "Anatolian Shepherd Dog"],
-  ["basenji", "Basenji"],
-  ["other", "Other"]
+  ['labrador', 'Labrador Retriever'],
+  ['frenchBulldog', 'French Bulldog'],
+  ['germanShepherdDog', 'German Shepherd Dog'],
+  ['goldenRetriever', 'Golden Retriever'],
+  ['bulldog', 'Bulldog'],
+  ['poodle', 'Poodle'],
+  ['beagle', 'Beagle'],
+  ['rottweiler', 'Rottweiler'],
+  ['germanShorthairedPointer', 'German Shorthaired Pointer'],
+  ['dachshund', 'Dachshund'],
+  ['pembrokeWelshCorgi', 'Pembroke Welsh Corgi'],
+  ['australianShepherd', 'Australian Shepherd'],
+  ['yorkshireTerrier', 'Yorkshire Terrier'],
+  ['boxer', 'Boxer'],
+  ['greatDane', 'Great Dane'],
+  ['siberianHuskie', 'Siberian Huskie'],
+  ['cavalierKingCharlesSpaniel', 'Cavalier King Charles Spaniel'],
+  ['dobermanPinscher', 'Doberman Pinscher'],
+  ['miniatureSchnauzer', 'Miniature Schnauzer'],
+  ['shihTzu', 'Shih Tzu'],
+  ['bostonTerrier', 'Boston Terrier'],
+  ['berneseMountainDog', 'Bernese Mountain Dog'],
+  ['pomeranian', 'Pomeranian'],
+  ['havanese', 'Havanese'],
+  ['caneCorso', 'Cane Corso'],
+  ['englishSpringerSpaniel', 'English Springer Spaniel'],
+  ['shetlandSheepdog', 'Shetland Sheepdog'],
+  ['brittany', 'Brittany'],
+  ['pug', 'Pug'],
+  ['cockerSpaniel', 'Cocker Spaniel'],
+  ['miniatureAmericanShepherd', 'Miniature American Shepherd'],
+  ['borderCollie', 'Border Collie'],
+  ['mastiff', 'Mastiff'],
+  ['chihuahua', 'Chihuahua'],
+  ['vizsla', 'Vizsla'],
+  ['bassetHound', 'Basset Hound'],
+  ['belgianMalinoi', 'Belgian Malinoi'],
+  ['maltese', 'Maltese'],
+  ['weimaraner', 'Weimaraner'],
+  ['collie', 'Collie'],
+  ['newfoundland', 'Newfoundland'],
+  ['rhodesianRidgeback', 'Rhodesian Ridgeback'],
+  ['shibaInu', 'Shiba Inu'],
+  ['westHighlandWhiteTerrier', 'West Highland White Terrier'],
+  ['bichonsFrise', 'Bichons Frise'],
+  ['bloodhound', 'Bloodhound'],
+  ['englishCockerSpaniels', 'English Cocker Spaniel'],
+  ['akita', 'Akita'],
+  ['portugueseWaterDog', 'Portuguese Water Dog'],
+  ['chesapeakeBayRetriever', 'Chesapeake Bay Retriever'],
+  ['dalmatian', 'Dalmatian'],
+  ['stBernard', 'St. Bernard'],
+  ['papillon', 'Papillon'],
+  ['australianCattleDog', 'Australian Cattle Dog'],
+  ['bullmastiff', 'Bullmastiff'],
+  ['samoyed', 'Samoyed'],
+  ['scottishTerrier', 'Scottish Terrier'],
+  ['softCoatedWheatenTerrier', 'Soft Coated Wheaten Terrier'],
+  ['whippet', 'Whippet'],
+  ['germanWirehairedpointer', 'German Wirehaired Pointer'],
+  ['chineseShar-Pei', 'Chinese Shar-Pei'],
+  ['airedaleTerrier', 'Airedale Terrier'],
+  ['wirehairedPointingGriffon', 'Wirehaired Pointing Griffon'],
+  ['bullTerrier', 'Bull Terrier'],
+  ['alaskanMalamute', 'Alaskan Malamute'],
+  ['cardiganWelshCorgi', 'Cardigan Welsh Corgi'],
+  ['giantSchnauzer', 'Giant Schnauzer'],
+  ['oldEnglishSheepdog', 'Old English Sheepdog'],
+  ['italianGreyhound', 'Italian Greyhound'],
+  ['greatPyrenee', 'Great Pyrenee'],
+  ['doguesdeBordeaux', 'Dogues de Bordeaux'],
+  ['russellTerrier', 'Russell Terrier'],
+  ['cairnTerrier', 'Cairn Terrier'],
+  ['irishWolfhound', 'Irish Wolfhound'],
+  ['irishsetter', 'Irish Setters'],
+  ['greaterSwissMountainDog', 'Greater Swiss Mountain Dog'],
+  ['miniaturePinscher', 'Miniature Pinscher'],
+  ['lhasaApso', 'Lhasa Apso'],
+  ['chineseCrested', 'Chinese Crested'],
+  ['cotondeTulear', 'Coton de Tulear'],
+  ['staffordshireBullTerrier', 'Staffordshire Bull Terrier'],
+  ['americanStaffordshireTerrier', 'American Staffordshire Terrier'],
+  ['ratTerrier', 'Rat Terrier'],
+  ['chowChow', 'Chow Chow'],
+  ['anatolianShepherdDog', 'Anatolian Shepherd Dog'],
+  ['basenji', 'Basenji'],
+  ['other', 'Other']
 ];
 
-const catBreedsMap = {
-  other: 'Other'
-};
+const catBreedsMap = [
+  ['ragdoll', 'Ragdoll'],
+  ['exoticShorthair', 'Exotic Shorthair'],
+  ['maineCoon', 'Maine Coon'],
+  ['persian', 'Persian'],
+  ['britishShorthair', 'British Shorthair'],
+  ['devonRex', 'Devon Rex'],
+  ['abyssinian', 'Abyssinian'],
+  ['americanShorthair', 'American Shorthair'],
+  ['scottish', 'Scottish Fold'],
+  ['sphynx', 'Sphynx'],
+  ['oriental', 'Oriental'],
+  ['siamese', 'Siamese'],
+  ['cornishRex', 'Cornish Rex'],
+  ['norwegianForestCat', 'Norwegian Forest Cat'],
+  ['siberian', 'Siberian'],
+  ['birman', 'Birman'],
+  ['russianBlue', 'Russian Blue'],
+  ['bengal', 'Bengal'],
+  ['burmese', 'Burmese'],
+  ['ocicat', 'Ocicat'],
+  ['other', 'Other']
+];
 
 const BrowsePetsPage = (props) => {
 
@@ -126,14 +146,14 @@ const BrowsePetsPage = (props) => {
     }
 
     const breedNamesMap = (inputs.type === 'dog') ? dogBreedsMap : catBreedsMap;
-    const options = Object.entries(breedNamesMap).map(([ breed, breedName ]) => {
+    const options = breedNamesMap.map(([ breed, displayName ]) => {
 
-      return <option value={breed} selected={inputs.breed === breed}>{breedName}</option>
+      return <option value={breed} selected={inputs.breed === breed}>{displayName}</option>
     });
     
     return (
       <FloatingLabel controlId="floatingSelect" label="Breed">
-        <Form.Select onChange={handleValueChange}>
+        <Form.Select onChange={handleValueChange} name="breed">
           {options}
         </Form.Select>
       </FloatingLabel>
@@ -145,7 +165,7 @@ const BrowsePetsPage = (props) => {
     return (
       <div className="root">
         <FloatingLabel controlId="floatingSelect" label="Type">
-          <Form.Select>
+          <Form.Select onChange={handleValueChange} name="type">
             <option value="dog" selected={inputs.type === 'dog'}>Dog</option>
             <option value="cat" selected={inputs.type === 'cat'}>Cat</option>
             <option value="other" selected={inputs.type === 'other'}>Other</option>
@@ -154,7 +174,7 @@ const BrowsePetsPage = (props) => {
         {breedSelect}
       </div>
     );
-  }, [breedSelect, inputs]);
+  }, [breedSelect, handleValueChange, inputs]);
 
   return componentOutput;
 }
