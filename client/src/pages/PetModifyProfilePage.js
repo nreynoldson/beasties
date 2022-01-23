12df17dee6e49cd88ac695ebca206893d98756ac
kruegead <kruegead@oregonstate.edu>
2022-01-23 08:18:43 -0600

    Move breed maps to consts file and add age input to pet profile

--- client/src/pages/PetModifyProfilePage.js
+++ client/src/pages/PetModifyProfilePage.js
@@ -1,6 +1,8 @@
 import { Fragment, useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router';
 
+import PetConsts from '../consts/Pets';
+
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
@@ -8,119 +10,6 @@ import Form from 'react-bootstrap/Form';
 import './css/Common.css';
 import './css/PetModifyProfilePage.css';
 
-const dogBreedsMap = [
-  ['labrador', 'Labrador Retriever'],
-  ['frenchBulldog', 'French Bulldog'],
-  ['germanShepherdDog', 'German Shepherd Dog'],
-  ['goldenRetriever', 'Golden Retriever'],
-  ['bulldog', 'Bulldog'],
-  ['poodle', 'Poodle'],
-  ['beagle', 'Beagle'],
-  ['rottweiler', 'Rottweiler'],
-  ['germanShorthairedPointer', 'German Shorthaired Pointer'],
-  ['dachshund', 'Dachshund'],
-  ['pembrokeWelshCorgi', 'Pembroke Welsh Corgi'],
-  ['australianShepherd', 'Australian Shepherd'],
-  ['yorkshireTerrier', 'Yorkshire Terrier'],
-  ['boxer', 'Boxer'],
-  ['greatDane', 'Great Dane'],
-  ['siberianHuskie', 'Siberian Huskie'],
-  ['cavalierKingCharlesSpaniel', 'Cavalier King Charles Spaniel'],
-  ['dobermanPinscher', 'Doberman Pinscher'],
-  ['miniatureSchnauzer', 'Miniature Schnauzer'],
-  ['shihTzu', 'Shih Tzu'],
-  ['bostonTerrier', 'Boston Terrier'],
-  ['berneseMountainDog', 'Bernese Mountain Dog'],
-  ['pomeranian', 'Pomeranian'],
-  ['havanese', 'Havanese'],
-  ['caneCorso', 'Cane Corso'],
-  ['englishSpringerSpaniel', 'English Springer Spaniel'],
-  ['shetlandSheepdog', 'Shetland Sheepdog'],
-  ['brittany', 'Brittany'],
-  ['pug', 'Pug'],
-  ['cockerSpaniel', 'Cocker Spaniel'],
-  ['miniatureAmericanShepherd', 'Miniature American Shepherd'],
-  ['borderCollie', 'Border Collie'],
-  ['mastiff', 'Mastiff'],
-  ['chihuahua', 'Chihuahua'],
-  ['vizsla', 'Vizsla'],
-  ['bassetHound', 'Basset Hound'],
-  ['belgianMalinoi', 'Belgian Malinoi'],
-  ['maltese', 'Maltese'],
-  ['weimaraner', 'Weimaraner'],
-  ['collie', 'Collie'],
-  ['newfoundland', 'Newfoundland'],
-  ['rhodesianRidgeback', 'Rhodesian Ridgeback'],
-  ['shibaInu', 'Shiba Inu'],
-  ['westHighlandWhiteTerrier', 'West Highland White Terrier'],
-  ['bichonsFrise', 'Bichons Frise'],
-  ['bloodhound', 'Bloodhound'],
-  ['englishCockerSpaniels', 'English Cocker Spaniel'],
-  ['akita', 'Akita'],
-  ['portugueseWaterDog', 'Portuguese Water Dog'],
-  ['chesapeakeBayRetriever', 'Chesapeake Bay Retriever'],
-  ['dalmatian', 'Dalmatian'],
-  ['stBernard', 'St. Bernard'],
-  ['papillon', 'Papillon'],
-  ['australianCattleDog', 'Australian Cattle Dog'],
-  ['bullmastiff', 'Bullmastiff'],
-  ['samoyed', 'Samoyed'],
-  ['scottishTerrier', 'Scottish Terrier'],
-  ['softCoatedWheatenTerrier', 'Soft Coated Wheaten Terrier'],
-  ['whippet', 'Whippet'],
-  ['germanWirehairedpointer', 'German Wirehaired Pointer'],
-  ['chineseShar-Pei', 'Chinese Shar-Pei'],
-  ['airedaleTerrier', 'Airedale Terrier'],
-  ['wirehairedPointingGriffon', 'Wirehaired Pointing Griffon'],
-  ['bullTerrier', 'Bull Terrier'],
-  ['alaskanMalamute', 'Alaskan Malamute'],
-  ['cardiganWelshCorgi', 'Cardigan Welsh Corgi'],
-  ['giantSchnauzer', 'Giant Schnauzer'],
-  ['oldEnglishSheepdog', 'Old English Sheepdog'],
-  ['italianGreyhound', 'Italian Greyhound'],
-  ['greatPyrenee', 'Great Pyrenee'],
-  ['doguesdeBordeaux', 'Dogues de Bordeaux'],
-  ['russellTerrier', 'Russell Terrier'],
-  ['cairnTerrier', 'Cairn Terrier'],
-  ['irishWolfhound', 'Irish Wolfhound'],
-  ['irishsetter', 'Irish Setters'],
-  ['greaterSwissMountainDog', 'Greater Swiss Mountain Dog'],
-  ['miniaturePinscher', 'Miniature Pinscher'],
-  ['lhasaApso', 'Lhasa Apso'],
-  ['chineseCrested', 'Chinese Crested'],
-  ['cotondeTulear', 'Coton de Tulear'],
-  ['staffordshireBullTerrier', 'Staffordshire Bull Terrier'],
-  ['americanStaffordshireTerrier', 'American Staffordshire Terrier'],
-  ['ratTerrier', 'Rat Terrier'],
-  ['chowChow', 'Chow Chow'],
-  ['anatolianShepherdDog', 'Anatolian Shepherd Dog'],
-  ['basenji', 'Basenji'],
-  ['other', 'Other']
-];
-
-const catBreedsMap = [
-  ['ragdoll', 'Ragdoll'],
-  ['exoticShorthair', 'Exotic Shorthair'],
-  ['maineCoon', 'Maine Coon'],
-  ['persian', 'Persian'],
-  ['britishShorthair', 'British Shorthair'],
-  ['devonRex', 'Devon Rex'],
-  ['abyssinian', 'Abyssinian'],
-  ['americanShorthair', 'American Shorthair'],
-  ['scottish', 'Scottish Fold'],
-  ['sphynx', 'Sphynx'],
-  ['oriental', 'Oriental'],
-  ['siamese', 'Siamese'],
-  ['cornishRex', 'Cornish Rex'],
-  ['norwegianForestCat', 'Norwegian Forest Cat'],
-  ['siberian', 'Siberian'],
-  ['birman', 'Birman'],
-  ['russianBlue', 'Russian Blue'],
-  ['bengal', 'Bengal'],
-  ['burmese', 'Burmese'],
-  ['ocicat', 'Ocicat'],
-  ['other', 'Other']
-];
 
 const PetModifyProfilePage = (props) => {
 
@@ -134,6 +23,7 @@ const PetModifyProfilePage = (props) => {
       name: '',
       type: 'other',
       breed: 'other',
+      age: 5,
       goodWithOtherAnimals: false,
       goodWithChildren: false,
       mustBeLeashed: false,
@@ -205,8 +95,8 @@ const PetModifyProfilePage = (props) => {
       return null;
     }
 
-    const breedNamesMap = (inputs.type === 'dog') ? dogBreedsMap : catBreedsMap;
-    const options = breedNamesMap.map(([ breed, displayName ]) => {
+    const breedNamesMap = (inputs.type === 'dog') ? PetConsts.dogBreedsMap : PetConsts.catBreedsMap;
+    const options = Object.entries(breedNamesMap).map(([ breed, displayName ]) => {
 
       return <option key={breed} value={breed}>{displayName}</option>;
     });
@@ -235,7 +125,19 @@ const PetModifyProfilePage = (props) => {
               type="text"
               onChange={handleValueChange}
               name="name"
+              value={inputs.name}
               placeholder="Name"
+              size="lg"
+              />
+          </FloatingLabel>
+
+          <FloatingLabel controlId="floatingInput" label="Age">
+            <Form.Control
+              type="number"
+              onChange={handleValueChange}
+              name="age"
+              value={inputs.age}
+              placeholder="Age"
               size="lg"
               />
           </FloatingLabel>
