a1ddf614b6e5ae926722714bb87e57e4fa9eff1b
nreynoldson <nicoleareynoldson@gmail.com>
2022-02-05 16:58:18 -0800

    removing extra lines from index

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.get('/api', (req,res) => {
     res.send("Api endpoint");
 });
 
-app.post('/user/:id', (req, res) => {
-    
-})
-
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
