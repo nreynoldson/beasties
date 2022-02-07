8ce259002d206ea0dcd466a173efb6897cc5c8c5
manickaa <manickaa@oregonstate.edu>
2022-02-06 21:50:37 -0800

    Handler files for getting user details by user name

--- backend/src/businessLogic/beastiesLogic.ts
+++ backend/src/businessLogic/beastiesLogic.ts
@@ -1,7 +1,8 @@
+import { APIGatewayProxyEvent } from 'aws-lambda'
 import { UserTableAccess } from '../dataLayer/userTableAccess'
 
 import { CreateUserAPIRequest } from '../requests/CreateUserAPIRequest'
-
+import { UserItem } from '../modals/UserItem'
 import { createLogger } from '../utils/logger'
 
 //generate BlogAccess and BlogS3Access object instance
@@ -14,9 +15,19 @@ export async function createUser(createUserRequest: CreateUserAPIRequest) : Prom
     const userItem = {
         ...createUserRequest
     }
-    logger.info('Adding a post to dynamodb')
+    logger.info('Adding a user to dynamodb')
     await userAccess.createUser(userItem);
 
     return userItem
+
+}
+
+export async function getUserByUsername(event: APIGatewayProxyEvent) : Promise<UserItem> {
+    
+    const userName = event.pathParameters.userName
+    logger.info(`Executing logic for get user with ${userName}`)
+    logger.info('Getting a user from dynamodb')
+    
+    return await userAccess.getUserByUsername(userName);
 
 }
---
+++ backend/src/lambda/http/getUserByUsername.ts
@@ -0,0 +1,29 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getUserByUsername } from '../../businessLogic/beastiesLogic'
+
+import {createLogger} from '../../utils/logger'
+
+const logger = createLogger('Get All Posts')
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  logger.info(`Processing Get a user by user name - ${JSON.stringify(event)}`)
+
+  const userDetails = await getUserByUsername(event)
+
+  logger.info(`Get user by user name succeeded. ${JSON.stringify(userDetails)}`)
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      items: userDetails
+    })
+  }
+  
+}
